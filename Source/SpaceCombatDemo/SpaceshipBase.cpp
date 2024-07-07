9862b4660281fad3d867d23b821220315b659e5f
TIMEMACHINE\chriswang <thebest1025w@hotmail.com>
2024-07-07 22:47:01 +0800

    Add components and enhance inputs

--- Source/SpaceCombatDemo/SpaceshipBase.cpp
+++ Source/SpaceCombatDemo/SpaceshipBase.cpp
@@ -2,6 +2,15 @@
 
 
 #include "SpaceshipBase.h"
+#include "Components/BoxComponent.h"
+#include "Components/ArrowComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "InputMappingContext.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "GameFramework/FloatingPawnMovement.h"
+
 
 // Sets default values
 ASpaceshipBase::ASpaceshipBase()
@@ -9,6 +18,23 @@ ASpaceshipBase::ASpaceshipBase()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	CollisonComp = CreateDefaultSubobject<UBoxComponent>(TEXT("CollisionRoot"));
+	SetRootComponent(CollisonComp);
+
+	ArrowComp = CreateDefaultSubobject<UArrowComponent>(TEXT("ArrowComp"));
+	ArrowComp->SetupAttachment(RootComponent);
+
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	MeshComp->SetupAttachment(RootComponent);
+
+	SpringArmFollow = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmFollow"));
+	SpringArmFollow->SetupAttachment(RootComponent);
+
+	CameraFollow = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraFollow"));
+	CameraFollow->SetupAttachment(SpringArmFollow);
+
+	FloatingMovement = CreateDefaultSubobject<UFloatingPawnMovement>(TEXT("SpaceshipMovement"));
+	
 }
 
 // Called when the game starts or when spawned
@@ -29,6 +55,36 @@ void ASpaceshipBase::Tick(float DeltaTime)
 void ASpaceshipBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(InputMapping, 0);
+		}
+	}
+
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ASpaceshipBase::MoveForward);
+		EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ASpaceshipBase::Look);
+		EnhancedInputComponent->BindAction(RollAction, ETriggerEvent::Triggered, this, &ASpaceshipBase::Roll);
+	}
+
+}
+
+void ASpaceshipBase::MoveForward(const FInputActionValue& Value)
+{
+
+}
+
+void ASpaceshipBase::Look(const FInputActionValue& Value)
+{
+
+}
+
+void ASpaceshipBase::Roll(const FInputActionValue& Value)
+{
 
 }
 
--- Source/SpaceCombatDemo/SpaceshipBase.h
+++ Source/SpaceCombatDemo/SpaceshipBase.h
@@ -6,6 +6,17 @@
 #include "GameFramework/Pawn.h"
 #include "SpaceshipBase.generated.h"
 
+class UBoxComponent;
+class USpringArmComponent;
+class UCameraComponent;
+class UStaticMeshComponent;
+class UInputAction;
+class UEnhancedInputComponent;
+struct FInputActionValue;
+class UArrowComponent;
+class UInputMappingContext;
+class UFloatingPawnMovement;
+
 UCLASS()
 class SPACECOMBATDEMO_API ASpaceshipBase : public APawn
 {
@@ -14,7 +25,6 @@ class SPACECOMBATDEMO_API ASpaceshipBase : public APawn
 public:
 	// Sets default values for this pawn's properties
 	ASpaceshipBase();
-
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -26,4 +36,39 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	void MoveForward(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+	void Roll(const FInputActionValue& Value);
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components")
+	TObjectPtr<UBoxComponent> CollisonComp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components")
+	TObjectPtr<UArrowComponent> ArrowComp;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components")
+	TObjectPtr<USpringArmComponent> SpringArmFollow;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components")
+	TObjectPtr<UCameraComponent> CameraFollow;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Components")
+	TObjectPtr<UStaticMeshComponent> MeshComp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	TObjectPtr<UInputAction> MoveAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	TObjectPtr<UInputAction> LookAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	TObjectPtr<UInputAction> RollAction;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	TObjectPtr<UInputMappingContext> InputMapping;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement")
+	TObjectPtr<UFloatingPawnMovement> FloatingMovement;
+	
 };
